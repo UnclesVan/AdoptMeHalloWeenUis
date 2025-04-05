bc75fd3937422152ceb2d4e20b6e0874f1b8371a
UnclesVan <mr.gremlingaming@gmail.com>
2025-04-05 13:08:10 +1100

    Update CandyUiLoader.lua

--- CandyUiLoader.lua
+++ CandyUiLoader.lua
@@ -419,7 +419,7 @@ local candyCorn = Instance.new("ImageLabel", frame)
 candyCorn.Size = UDim2.new(0.35, 0, 0.9, 0)
 candyCorn.Position = UDim2.new(0.02, 0, 0.05, 0)
 candyCorn.BackgroundTransparency = 1
-candyCorn.Image = "rbxassetid://106495507831851"  -- Reference image ID    
+candyCorn.Image = "rbxassetid://85161010184505"  -- Reference image ID    
 candyCorn.ScaleType = Enum.ScaleType.Fit
 
 --all old image IDs from Adopt Me! Devs:
@@ -427,7 +427,7 @@ candyCorn.ScaleType = Enum.ScaleType.Fit
 --Stars ID: 80184394321521 patched.
 -- 80184394321521 patched.
 -- Hearts 86091048492397 patched
-
+-- 106495507831851 shards patched
 
 -- Create the "READY FOR CHRISTMAS" message on top of the candy corn image
 local readyForChristmasLabel = Instance.new("TextLabel", frame)
